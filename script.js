e4f1a205f957a8dc2b52acd1e64b53d34d5a726f
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2021-02-03 14:42:19 +0000

    Updated script.js
    Implemented response in catch code, now returns will No Quote response.

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ async function getQuote() {
     } 
     catch (err){
         console.log("Whoops, no quote...", err)
+        quoteDOM.innerHTML = "'Whoops, no quote...''"
+        autherDOM.innerHTML = "Unknown"
+        loaded()
     }
 }
 
